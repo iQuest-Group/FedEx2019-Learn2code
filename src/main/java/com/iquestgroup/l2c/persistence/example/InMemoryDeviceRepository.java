fb9d4c4a6ea382462d09db6553b93e853d9fbc75
Teodora Tanase <teodora.tanase@iquestgroup.com>
2019-11-22 14:38:41 +0200

    Basic setup for repository and in-memory implementation

---
+++ src/main/java/com/iquestgroup/l2c/persistence/example/InMemoryDeviceRepository.java
@@ -0,0 +1,50 @@
+package com.iquestgroup.l2c.persistence.example;
+
+import com.iquestgroup.l2c.model.Device;
+import com.iquestgroup.l2c.persistence.DeviceRepository;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Optional;
+import java.util.Random;
+
+@Repository
+public class InMemoryDeviceRepository implements DeviceRepository {
+
+  private static final Random RANDOM = new Random();
+
+  private final Collection<Device> devices;
+
+  public InMemoryDeviceRepository() {
+    devices = new ArrayList<>();
+  }
+
+  @Override
+  public Device save(Device device) {
+    if (device == null) {
+      throw new IllegalArgumentException("Device must not be null!");
+    }
+
+    device.setId(RANDOM.nextLong());
+    devices.add(device);
+    return device;
+  }
+
+  @Override
+  public Optional<Device> findById(long id) {
+    return devices.stream()
+        .filter(device -> id == device.getId())
+        .findFirst();
+  }
+
+  @Override
+  public void delete(Device device) {
+    if (device == null) {
+      throw new IllegalArgumentException("Device must not be null!");
+    }
+
+    devices.remove(device);
+  }
+}
