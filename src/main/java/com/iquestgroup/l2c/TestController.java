08fae6c73bef5f6b2e739b495c870ad4c5f54805
Bogdan Socaciu <bogdan.socaciu@iquestgroup.com>
2019-11-22 14:06:00 +0200

    Added web resources + event table

--- src/main/java/com/iquestgroup/l2c/TestController.java
+++ src/main/java/com/iquestgroup/l2c/TestController.java
@@ -1,22 +1,34 @@
 package com.iquestgroup.l2c;
 
+import com.iquestgroup.l2c.event.DeviceEvent;
+import com.iquestgroup.l2c.event.DeviceEventType;
+import com.iquestgroup.l2c.event.SourceType;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+import java.time.ZonedDateTime;
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 public class TestController {
-	// inject via application.properties
-	@Value("${welcome.message}")
-	private String message;
+    // inject via application.properties
+    @Value("${welcome.message}")
+    private String message;
 
-	@GetMapping("/")
-	public String index(Model model) {
+    private List<DeviceEvent> events = Arrays.asList(
+            new DeviceEvent("test-1231dad1", 11512972L, ZonedDateTime.now(), "Detailing details", DeviceEventType.PICTURE_TAKEN, SourceType.CAMERA),
+            new DeviceEvent("test-cffcdad1", 71816972L, ZonedDateTime.now(), "Detailing another details", DeviceEventType.SPEAKER_OFF, SourceType.SPEAKER)
+    );
 
-		model.addAttribute("message", message);
+    @GetMapping("/")
+    public String index(Model model) {
 
-		return "tmpl"; //view
-	}
+        model.addAttribute("message", message);
+        model.addAttribute("events", events);
+
+        return "tmpl"; //view
+    }
 }
--- src/main/java/com/iquestgroup/l2c/event/DeviceEvent.java
+++ src/main/java/com/iquestgroup/l2c/event/DeviceEvent.java
@@ -1,10 +1,14 @@
 package com.iquestgroup.l2c.event;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.ZonedDateTime;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class DeviceEvent {
     private String eventId;
     private Long deviceId;
