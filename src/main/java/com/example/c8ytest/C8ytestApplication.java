3fa91a69d5ad7da34d1db564f85ca6b1e19a6400
Zoltan Antal <zoltan.antal@iquestgroup.com>
2019-11-22 10:43:03 +0200

    Initial commit with code skeleton Spring Boot code

---
+++ src/main/java/com/example/c8ytest/C8ytestApplication.java
@@ -0,0 +1,12 @@
+package com.example.c8ytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C8ytestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(C8ytestApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/c8ytest/C8ytestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.c8ytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C8ytestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
