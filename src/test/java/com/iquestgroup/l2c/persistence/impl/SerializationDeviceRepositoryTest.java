24b297cfcf4c5bdf7e59daf69877a42be68f2adc
Teodora Tanase <teodora.tanase@iquestgroup.com>
2019-11-25 11:37:10 +0200

    Fixed test configuration

--- src/test/java/com/iquestgroup/l2c/persistence/impl/SerializationDeviceRepositoryTest.java
+++ src/test/java/com/iquestgroup/l2c/persistence/impl/SerializationDeviceRepositoryTest.java
@@ -4,12 +4,10 @@ import com.iquestgroup.l2c.model.Device;
 import com.iquestgroup.l2c.persistence.DeviceRepository;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.Optional;
@@ -18,8 +16,12 @@ import java.util.Optional;
 @ExtendWith(SpringExtension.class)
 public class SerializationDeviceRepositoryTest {
 
-  @Autowired
   private DeviceRepository deviceRepository;
+
+  @BeforeEach
+  public void setup() {
+    deviceRepository = new SerializationDeviceRepository();
+  }
 
   @Test
   public void testShouldNotSaveIfDeviceIsNull() {
@@ -60,13 +62,5 @@ public class SerializationDeviceRepositoryTest {
     Optional<Device> result = deviceRepository.findById(id);
 
     Assertions.assertTrue(result.isEmpty());
-  }
-
-  @TestConfiguration
-  static class SerializationRepositoryTestConfiguration {
-    @Bean
-    public DeviceRepository deviceRepository() {
-      return new SerializationDeviceRepository();
-    }
   }
 }
